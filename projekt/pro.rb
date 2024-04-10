02b553e7ef28e8d244ea258d188893f5c146d04d
Gianne the Weirdo <118273693+giang-doan-ntijohanneberg@users.noreply.github.com>
2024-04-10 04:23:19 +0200

    .

--- projekt/pro.rb
+++ projekt/pro.rb
@@ -26,8 +26,6 @@ get('/') do
     slim(:index)
 end
 
-
-
 get('/profile') do
     user_id = session[:id].to_i
     db = connect_to_db('db/projekt.db')
@@ -69,7 +67,20 @@ post('/addclothes') do
     color_id = params[:color_picker]
     size = params[:size]
     notes = params[:notes]
-    image = params[:image][:tempfile].read
+
+    if params[:image] && params[:image][:tempfile]
+        image = params[:image][:tempfile].read
+    else
+        flash[:message] = "Please upload an image"
+        redirect('/add')
+        return
+    end
+
+    if season_id == 0 || pattern_id == 0 || cloth_name.nil? || cloth_name.empty? || type_id == 0 || color_id.nil? || color_id.empty? || size.nil? || size.empty? || notes.nil? || notes.empty? || image.nil?
+        flash[:message] = "Please fill out all the fields!"
+        redirect('/add')
+        return
+    end
 
     db = connect_to_db('db/projekt.db')
     db.execute("INSERT INTO wardrobe (user_id, season_id, cloth_name, type_id, size, notes, image, pattern_id, color_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", user_id, season_id, cloth_name, type_id, size, notes, image, pattern_id, color_id)
@@ -157,7 +168,7 @@ post('/filter') do
     end
 
 
-    if color_id && !color_id.empty?
+    if color_id && !color_id.empty? && color_id != "#000000"
         conditions << "wardrobe.color_id =?"
         filter_params << color_id
     end
@@ -218,15 +229,21 @@ post('/login') do
     db = connect_to_db('db/projekt.db')
     result = db.execute("SELECT * FROM users WHERE username = ?", username).first
     if result.nil?
-        "Username does not exist"
+        flash[:message] = "Username does not exist"
+        redirect('/member')
     else
         pwdigest = result["pwdigest"]
         if BCrypt::Password.new(pwdigest) == password
             session[:id] = result["id"]
             session[:username] = result["username"]
-            redirect('/profile')
+            if username == "admin"
+                redirect('/admin')
+            else
+                redirect('/profile')
+            end
         else
-            "Wrong password"
+            flash[:message] = "Wrong password!"
+            redirect('/member')
         end
     end
 end
@@ -235,4 +252,94 @@ get('/logout') do
     session.clear
     flash[:notice] = "You have been logged out"
     redirect('/')
+end
+
+get('/admin') do
+    db = connect_to_db('db/projekt.db')
+    seasons = db.execute("SELECT * FROM season")
+    clothtypes = db.execute("SELECT * FROM clothtypes")
+    patterns = db.execute("SELECT * FROM pattern")
+
+    items = db.execute("SELECT wardrobe.*, season.season_name, clothtypes.type_name, pattern.pattern_name FROM wardrobe JOIN season ON wardrobe.season_id = season.season_id JOIN clothtypes ON wardrobe.type_id = clothtypes.type_id JOIN pattern ON wardrobe.pattern_id = pattern.pattern_id")
+
+    if items.empty?
+        return slim(:"admin/admin", locals:{patterns:patterns, seasons:seasons, clothtypes:clothtypes, items:items, message: "Nothing is added."})
+    else
+        slim(:"admin/admin", locals:{patterns:patterns, seasons:seasons, clothtypes:clothtypes, items:items})
+    end
+
+end
+
+get('/admin/edit-user') do
+    db = connect_to_db('db/projekt.db')
+    users = db.execute("SELECT * FROM users")
+    slim(:"admin/edituser", locals: {users: users})
+end
+
+
+get('/admin/:id/show') do
+    user_id = params[:id].to_i
+    db = connect_to_db('db/projekt.db')
+    users = db.execute("SELECT * FROM users")
+    seasons = db.execute("SELECT * FROM season")
+    clothtypes = db.execute("SELECT * FROM clothtypes")
+    patterns = db.execute("SELECT * FROM pattern")
+
+    @username = db.execute("SELECT username FROM users WHERE id = ?", user_id).first['username']
+
+
+    items = db.execute("SELECT wardrobe.*, season.season_name, clothtypes.type_name, pattern.pattern_name FROM wardrobe JOIN season ON wardrobe.season_id = season.season_id JOIN clothtypes ON wardrobe.type_id = clothtypes.type_id JOIN pattern ON wardrobe.pattern_id = pattern.pattern_id WHERE wardrobe.user_id = ?", user_id)
+
+    if items.empty?
+        return slim(:"admin/edituser", locals:{patterns:patterns, seasons:seasons, clothtypes:clothtypes, items:items, users:users, message: "There is nothing to show."})
+    else
+        slim(:"admin/show", locals:{patterns:patterns, seasons:seasons, clothtypes:clothtypes, users:users, items:items})
+    end
+end
+
+
+post('/admin/:id/delete') do
+    user_id = params[:id].to_i
+    db = connect_to_db('db/projekt.db')
+    db.execute("DELETE FROM users WHERE id=?", user_id)
+    redirect('/admin/edit-user')
+end
+
+get('/admin/edit-data') do
+    db = connect_to_db('db/projekt.db')
+    seasons = db.execute("SELECT * FROM season")
+    clothtypes = db.execute("SELECT * FROM clothtypes")
+    patterns = db.execute("SELECT * FROM pattern")
+    slim(:"admin/editdata", locals: {seasons:seasons, clothtypes:clothtypes, patterns:patterns})
+end
+
+post('/admin/edit-data') do
+    season = params[:season_name]
+    clothtype = params[:type_name]
+    pattern = params[:pattern_name]
+    db = connect_to_db('db/projekt.db')
+    if season && !season.empty?
+        db.execute("INSERT INTO season (season_name) VALUES(?)", season)
+    end
+
+    if clothtype && !clothtype.empty?
+    db.execute("INSERT INTO clothtypes (type_name) VALUES(?)", clothtype)
+    end
+
+    if pattern && !pattern.empty?
+    db.execute("INSERT INTO pattern (pattern_name) VALUES(?)", pattern)
+    end
+
+    redirect('/admin/edit-data')
+end
+
+post('/admin/:id/deletedata') do
+    season_id = params[:season_id].to_i
+    clothtype_id = params[:type_id].to_i
+    pattern_id = params[:pattern_id].to_i
+    db = connect_to_db('db/projekt.db')
+    db.execute("DELETE FROM season WHERE season_id=?", season_id)
+    db.execute("DELETE FROM clothtypes WHERE type_id=?", clothtype_id)
+    db.execute("DELETE FROM pattern WHERE pattern_id=?", pattern_id)
+    redirect('/admin/edit-data')
 end
